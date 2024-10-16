219693f6bb1623bcb1d2cc3ce3e075d222b5357c
Megan Palmquist <megpalmq@iu.edu>
2024-10-16 09:53:50 -0400

    fixed files! (nav)

---
+++ src/components/Nav/index.js
@@ -0,0 +1,19 @@
+import navStyles from "./nav.module.css";
+
+export default function Nav() {
+  return (
+    <nav className={navStyles.mainNav}>
+      <ul>
+        <li>
+          <a href="/">Home🏠</a>
+        </li>
+        <li>
+          <a href="/">About❓</a>
+        </li>
+        <li>
+          <a href="/">Contact📨</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
