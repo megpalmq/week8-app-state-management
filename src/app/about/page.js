51a67737e84d2e14f0f08eaaa842eaf523ec2d77
Megan Palmquist <megpalmq@iu.edu>
2024-10-23 10:31:55 -0400

    added about field

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,8 +1,12 @@
 "use client";
 import useAppState from "../../hooks/useAppState";
+import {useState} from "react";
 
 export default function About() {
    const appState = useAppState();
+   function changeFormField(e){
+    setNewPersonForm({...newPersonForm, [e.currentTarget.name]: e.currentTarget.value})
+   }
 
   console.log(appState);
 
@@ -10,6 +14,16 @@ export default function About() {
     
       <main>
         <h1>About</h1>
+
+        <form>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input id="name" name="name" value={newPersonForm.name} onChange={changeFormField}></input>
+          </div>
+          <div>
+            <input type="submit" value="Add Person"/>
+          </div>
+        </form>
       </main>
   );
 }
